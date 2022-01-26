e853d3c73f309ca6da2b611d3f00c0c38411764d
Kitzia Vidal <kitpao91@gmail.com>
2022-01-26 14:48:36 -0600

    change functionality in button, grab an input

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,13 +2,24 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = e => {
+    // the form should not submit
+    e.preventDefault();
+    // should get the input
+    console.log(this.myInput);
+    // go to the /store/input
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         { /* This is a comment in js, because I'm in jsx */}
         <h2>Please Enter A Store</h2>
         <input
           type="text"
+          ref={this.myInput}
           required
           placeholder="Store Name"
           defaultValue={getFunName()}
