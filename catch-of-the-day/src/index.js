53d3d7f1517502ade0acaa9718e5365f4e37cbdc
Kitzia Vidal <kitpao91@gmail.com>
2022-01-26 09:42:25 -0600

    Create an App with Subcomponents

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -4,5 +4,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import './css/style.css';
 import StorePicker from './components/StorePicker';
+import App from './components/App';
 
-render(<StorePicker />, document.querySelector('#main'));
+
+//render(<StorePicker />, document.querySelector('#main'));
+render(<App />, document.querySelector('#main'));
