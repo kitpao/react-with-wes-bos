5e7591ae2df979dd470b52c8f44083ec885a9fa0
Kitzia Vidal <kitpao91@gmail.com>
2022-01-31 17:15:52 -0600

    Calculate Order Total

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { formatPrice } from '../helpers';
 
 class Order extends React.Component {
   render() {
+    const orderIds = Object.keys(this.props.order);
+    const total = orderIds.reduce((prevTotal,fish) => {
+      const isAvailable = fish && this.props.fishes[fish].status === "available";
+      const fishPrice = this.props.fishes[fish].price;
+      const fishCounter = this.props.order[fish];
+      if(isAvailable){
+        return prevTotal + (fishPrice * fishCounter)
+      }
+      return prevTotal
+    }, 0)
+
     return (
-      <div className="order">
-        Order!
+      <div className="order-wrap">
+        <h2>Order</h2>
+        {orderIds}
+        <div className="total">
+          Total:
+          <strong>{formatPrice(total)}</strong>
+        </div>
       </div>
     )
   }
