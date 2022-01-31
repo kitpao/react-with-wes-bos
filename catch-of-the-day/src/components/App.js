7912f256ee08f94524f5e055c9c45e86f6530002
Kitzia Vidal <kitpao91@gmail.com>
2022-01-31 15:58:59 -0600

    Display the list of fishes

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -3,6 +3,7 @@ import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 import fishes from '../sample-fishes'
+import Fish from './Fish';
 
 class App extends React.Component {
   state={
@@ -30,6 +31,15 @@ class App extends React.Component {
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline = "Fresh Seafood Market" />
+          <ul className="fishes"> {/*loop through fishes*/}
+            { Object.keys(this.state.fishes).map( key =>
+              <Fish
+                key={key}
+                details={this.state.fishes[key]}
+              />
+            )}
+
+          </ul>
         </div>
         <Order />
         <Inventory
