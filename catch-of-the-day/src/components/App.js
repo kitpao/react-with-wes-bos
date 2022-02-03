58b45b6c404c7147bc60573074be3779acfb0600
Kitzia Vidal <kitpao91@gmail.com>
2022-02-03 13:53:32 -0600

    sync with firebase

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,14 +2,27 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
-import fishes from '../sample-fishes'
+import fishes from '../sample-fishes';
 import Fish from './Fish';
+import base from '../base';
 
 class App extends React.Component {
   state={
     fishes: {},
     order: {},
   };
+
+  componentDidMount() {
+    const { params } = this.props.match;
+    this.ref = base.syncState(`${params.storeID}/fishes`,{
+      context: this,
+      state: 'fishes'
+    });
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref)
+  }
 
   addFish = fish => { // To change state,
     // First create a copy
